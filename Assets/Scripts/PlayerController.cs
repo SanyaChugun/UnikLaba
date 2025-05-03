08d0b825efb5c79833d69fd5bb176f4d3e479591
SanyaChugun <sashkachugun@gmail.com>
2025-05-03 12:57:25 +0400

    Camera+Friend
    Add MoveCamera script
    Add Friend
    Add Friend func in Player script

--- Assets/Scripts/PlayerController.cs
+++ Assets/Scripts/PlayerController.cs
@@ -2,12 +2,29 @@ using System;
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
+using static UnityEngine.Rendering.DebugUI;
+
 [RequireComponent(typeof(Rigidbody), typeof(BoxCollider))]
 public class PlayerController : MonoBehaviour
 {
+    [SerializeField] TextMeshProUGUI forceText;
     [SerializeField] private Rigidbody _rigidbody;
     [SerializeField] private FixedJoystick _joystick;
     [SerializeField] private float _moveSpeed;
+    public int health = 0;
+
+    private void OnCollisionEnter(Collision collision)
+    {
+        if (collision.gameObject.tag == "Friend")
+        {
+// Friend friend = collision.gameObject.GetComponent<Friend>();
+ //          int healthFriend = friend._health;
+   //         health += healthFriend;
+            forceText.text = health.ToString();
+            Destroy(collision.gameObject);
+        }
+    }
     private void FixedUpdate()
     {
         _rigidbody.linearVelocity = new Vector3(_joystick.Horizontal * _moveSpeed, _rigidbody.linearVelocity.y, _joystick.Vertical * _moveSpeed);
